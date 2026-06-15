ce711c206dd00e09f714eaba0d12b6abe9a5498c
Iron <ardilla_conker1@hotmail.com>
2026-06-15 14:32:54 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,44 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements AfterViewInit{
+  result : any[] =  [];
+
+   ngAfterViewInit() {
+    this.informacion();
+  }
+
+  
+  async informacion() {
+    try {
+      const response = await fetch('http://localhost:8085/ejercicio/api.php', {
+        method: 'GET',
+        headers: { 'Origin': window.location.origin },
+      });
+
+      this.result = await response.json();
+      
+
+    } catch (error) {
+      console.error('Error peticion:', error);
+    }
+  }
+
+  agregar(nombre:any, apellidos: any, edad:any){
+    const ultimoId = this.result[this.result.length - 1].dato;
+    
+    this.result.push({
+      dato: Number(ultimoId) + 1,
+      nombre: nombre,
+      apellidos: apellidos,
+      edad: edad
+    });
+  }
+
+  eliminar(a:any){
+    this.result.splice(a, 1);
+  }
+
 }
+
+
